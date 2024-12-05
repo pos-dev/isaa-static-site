28ff5c13df6abd3eded85a34fb8b8cb5e559d27a
Poese Toomata <toomatapoese@gmail.com>
2024-12-05 14:33:01 +0800

    first deployment

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-30.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-0e08d916', {"layout":"number","heading":"Schools","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":20,"totalNumber":100,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-88f79663', {"layout":"number","heading":"Athletes","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":1000,"totalNumber":1000,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-096eefd6', {"layout":"number","heading":"Presenters","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":10,"totalNumber":100,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
